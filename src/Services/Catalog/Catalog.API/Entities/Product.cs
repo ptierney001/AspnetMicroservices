3ef31fe265b1f56a7fb3dc693ff3658b57e1a1b8
Paul Tierney <paul.tierney@pwc.com>
2022-07-19 10:00:16 +0100

    Added mongo nuget

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        public string Id { get; set; }
+    }
+}
