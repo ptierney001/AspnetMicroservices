2845b98499cfcc17321a47b4bf43fbb687b42306
Paul Tierney <paul.tierney@pwc.com>
2022-08-19 12:03:33 +0100

    Added implementation of consuming and subscribing for basketcheckout-queue RabbitMQ

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -27,6 +27,10 @@ builder.Services.AddMassTransit(config =>
     });
 });
 
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddScoped<BasketCheckoutConsumer>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
